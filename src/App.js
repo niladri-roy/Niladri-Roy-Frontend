a06117d58cf58d8dcd4f689f8ade2284fabbe741
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2023-07-03 00:48:56 +0530

    scroll bar customize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Routes, Route } from 'react-router-dom';
 import './App.css';
+import './scrollBar.css';
 import Header from './components/Header/Header';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
